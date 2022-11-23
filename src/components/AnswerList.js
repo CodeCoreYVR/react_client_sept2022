3775d51b9133cec70bc5a33f2f1b9264d72787d6
Stephanie <steph_war@yahoo.ca>
2022-11-23 13:18:13 -0800

    react client React Fragment

--- src/components/AnswerList.js
+++ src/components/AnswerList.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import AnswerDetails from './AnswerDetails';
 
 const AnswerList = ({answers}) => {
     return(
-        <div>
+        <React.Fragment>
             {
                 answers.map((a,i) => {
                     return <AnswerDetails
@@ -13,9 +14,18 @@ const AnswerList = ({answers}) => {
                     />
                 })
             }
-        </div>
+        </React.Fragment>
     )
 }
+
+//React render can only render one component at a time, and to bypass that we created parent elements
+//to contain a bunch of sibling elements, like the iterated AnswerDetails elements within a div in the 
+//AnswerList component.  However, if you don't want to specifically create extra html elements just to 
+//contain these elements (for example, a div element could create additional unwanted spacing and margins), React has other 
+//ways you can manage:
+//React.Fragment is a special component that you can use if you want to list a bunch of sibling
+//elements but do not want to specially create a container for them
+//You can use <React.Fragment></React.Fragment> or shorthand is: <></>
 
 export default AnswerList;
 
