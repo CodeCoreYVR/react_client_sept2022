76252ffb825f0e2d1660e4c1c79d570f22b69e2d
Stephanie <steph_war@yahoo.ca>
2022-11-24 09:48:30 -0800

    react state change QuestionShowPage to class component

--- src/components/QuestionShowPage.js
+++ src/components/QuestionShowPage.js
@@ -1,42 +1,45 @@
 import QuestionDetails from './QuestionDetails';
 // import AnswerDetails from './AnswerDetails';
 import AnswerList from './AnswerList';
+import { Component } from 'react';
 
-const QuestionShowPage = () => {
-    //This component will have one main element, with two children elements
-    return(
-      <main>
-        <QuestionDetails
-        title="What is your favourite colour?"
-        body="Red,green,blue,rainbow..."
-        author={{full_name: "Jon Snow"}}
-        view_count={35}
-        created_at={ new Date() }
-        updated_at={ new Date() }
-        />
-        <AnswerList
-        answers={
-            [
-                {
-                    body: "Answer 1",
-                    author: { full_name: "Steve Jobs" },
-                    created_at: new Date()
-                },
-                {
-                    body: "Answer 2",
-                    author: { full_name: "Elon Musk" },
-                    created_at: new Date()
-                },
-                {
-                    body: "Answer 3",
-                    author: { full_name: "Jeff Bezos" },
-                    created_at: new Date()
-                },
-            ]
-        }
-        />
-      </main>
-    )
+class QuestionShowPage extends Component {
+    render(){
+        return(
+          <main>
+            <QuestionDetails
+            title="What is your favourite colour?"
+            body="Red,green,blue,rainbow..."
+            author={{full_name: "Jon Snow"}}
+            view_count={35}
+            created_at={ new Date() }
+            updated_at={ new Date() }
+            />
+            <AnswerList
+            answers={
+                [
+                    {
+                        body: "Answer 1",
+                        author: { full_name: "Steve Jobs" },
+                        created_at: new Date()
+                    },
+                    {
+                        body: "Answer 2",
+                        author: { full_name: "Elon Musk" },
+                        created_at: new Date()
+                    },
+                    {
+                        body: "Answer 3",
+                        author: { full_name: "Jeff Bezos" },
+                        created_at: new Date()
+                    },
+                ]
+            }
+            />
+          </main>
+        )
+    }
+    
 }
 
 export default QuestionShowPage;
