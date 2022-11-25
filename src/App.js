5f00a1d383474899eda9673aadfe4b90333f3ff9
Stephanie <steph_war@yahoo.ca>
2022-11-25 10:50:43 -0800

    react client http session request, hacky sign in, default port 5500

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
 import { Component } from 'react';
 import CurrentDateTime from './components/CurrentDateTime';
+import {Session} from './requests';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      clocksCount: [1] //an array of something
+      clocksCount: [1], //an array of something
+      user: null
     }
+  }
+
+  componentDidMount(){
+    Session.create(
+      {
+        email: "admin@user.com",
+        password: "123"
+      }
+    ).then( fetchedUser => {
+      this.setState((state) => {
+        return{
+          user: fetchedUser
+        }
+      })
+    })
   }
   render(){
     return (
