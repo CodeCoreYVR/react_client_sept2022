e838cad7ed0a42c9a0ec852d0976180ce844a975
Matt <mattcdneale@gmail.com>
2022-11-29 10:04:29 -0800

    added use state for errors on sign in page

--- src/components/SignInPage.js
+++ src/components/SignInPage.js
@@ -1,50 +1,62 @@
-import { Session } from '../requests'
-import { useNavigate } from 'react-router-dom'
+import { Session } from "../requests";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function SignInPage(props) {
-  const { onSignIn } = props
-  
-  const navigate = useNavigate()
+  const { onSignIn } = props;
+
+  const navigate = useNavigate();
+
+  const [errors, setErrors] = useState([]);
 
   function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
     const { currentTarget } = event;
 
-    const formData = new FormData(currentTarget)
+    const formData = new FormData(currentTarget);
     const params = {
-      email: formData.get('email'),
-      password: formData.get('password')
-    }
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
 
-    Session.create(params).then(data => {
-      if (data.id) {
-        onSignIn()
+    Session.create(params).then((data) => {
+      if (data.status === 404) {
+        setErrors([...errors, { message: "Wrong email or password" }]);
+      } else if (data.id) {
+        onSignIn();
         // Navigated to Index page from the browser
         // We can 'push' on history to manipulate the browser
         // and direct our user to any page in our app
         // props.history.push('/questions') <--- not working
-        navigate('/questions')
+        navigate("/questions");
       }
-    })
+    });
   }
 
-  return(
+  return (
     <main>
       <h1>Sign In</h1>
       <form onSubmit={handleSubmit}>
+        {errors.length > 0 ? (
+          <div>
+            <h4>Failed to Sign In</h4>
+            <p>{errors.map((error) => error.message).join(", ")}</p>
+          </div>
+        ) : (
+          ""
+        )}
         <div>
-          <label htmlFor='email'>Email</label>
+          <label htmlFor="email">Email</label>
           <input type="text" name="email" id="email" />
         </div>
         <div>
-          <label htmlFor='password'>Password</label>
+          <label htmlFor="password">Password</label>
           <input type="password" name="password" id="password" />
         </div>
         <input type="submit" value="Sign In" />
       </form>
     </main>
-  )
+  );
 }
 
-export default SignInPage
-
+export default SignInPage;
