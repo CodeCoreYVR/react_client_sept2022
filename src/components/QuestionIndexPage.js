942d09075c8719342a3d1947906b1698595b9ddf
Matt <mattcdneale@gmail.com>
2022-11-29 11:44:19 -0800

    refactored question index page with hooks

--- src/components/QuestionIndexPage.js
+++ src/components/QuestionIndexPage.js
@@ -1,53 +1,91 @@
 // import QuestionDetails from './QuestionDetails';
-import {Component} from 'react';
+import { useState, useEffect } from "react";
 // import questionIndexData from '../questionIndexData';
 // import NewQuestionForm from './NewQuestionForm';
-import {Question} from '../requests';
-import {Link} from 'react-router-dom';
+import { Question } from "../requests";
+import { Link } from "react-router-dom";
 
-class QuestionIndexPage extends Component {
-    // state = {
-    //     questions: questionIndexData
-    // }
-    constructor(props){
-        super(props);
-        this.state = { questions: []}
-    }
+export default function QuestionIndexPage() {
+  const [questions, setQuestions] = useState([]);
 
-    componentDidMount(){
-        Question.index()
-        .then((questionsData) => {
-            this.setState(() => {
-                return{
-                    questions: questionsData
-                }
-            })
-        })
-    }
+  useEffect(() => {
+    Question.index().then((questionsData) => {
+      setQuestions(questionsData);
+    });
+  }, []);
 
-    deleteQuestion(id){
-        console.log(id)
-        //we can;t reset the value of state in this way:
-        //this.state.questions = this.state.questions.filter(q => q.id != id)
-        //you can only reset the value by using this.setState()
-        this.setState({
-           questions: this.state.questions.filter(q => q.id != id) 
-        })
-    }
+  const deleteQuestion = (id) => {
+    console.log(id);
+    //we can;t reset the value of state in this way:
+    //this.state.questions = this.state.questions.filter(q => q.id != id)
+    //you can only reset the value by using this.setState()
 
-    render(){
-        return(
-            <>
-                <ul>
-                    {
-                        this.state.questions.map((q, i) => {
-                            return <li key={i}>{q.id} - <Link to={`/questions/${q.id}`}>{q.title}</Link> <button onClick={()=> {this.deleteQuestion(q.id)}}>Delete</button></li>
-                        })
-                    }
-                </ul>
-            </>
-        )
-    }
+    setQuestions(questions.filter((q) => q.id !== id));
+  };
+
+  return (
+    <>
+      <ul>
+        {questions.map((q, i) => {
+          return (
+            <li key={i}>
+              {q.id} - <Link to={`/questions/${q.id}`}>{q.title}</Link>{" "}
+              <button
+                onClick={() => {
+                  deleteQuestion(q.id);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
-export default QuestionIndexPage;
+// class QuestionIndexPage extends Component {
+//     // state = {
+//     //     questions: questionIndexData
+//     // }
+//     constructor(props){
+//         super(props);
+//         this.state = { questions: []}
+//     }
+
+//     componentDidMount(){
+//         Question.index()
+//         .then((questionsData) => {
+//             this.setState(() => {
+//                 return{
+//                     questions: questionsData
+//                 }
+//             })
+//         })
+//     }
+
+//     deleteQuestion(id){
+//         console.log(id)
+//         //we can;t reset the value of state in this way:
+//         //this.state.questions = this.state.questions.filter(q => q.id != id)
+//         //you can only reset the value by using this.setState()
+//         this.setState({
+//            questions: this.state.questions.filter(q => q.id != id)
+//         })
+//     }
+
+//     render(){
+//         return(
+//             <>
+//                 <ul>
+//                     {
+//                         this.state.questions.map((q, i) => {
+//                             return <li key={i}>{q.id} - <Link to={`/questions/${q.id}`}>{q.title}</Link> <button onClick={()=> {this.deleteQuestion(q.id)}}>Delete</button></li>
+//                         })
+//                     }
+//                 </ul>
+//             </>
+//         )
+//     }
+// }
