1f8123a802f6208cb63872d9f96d5f50e06b101b
Matt <mattcdneale@gmail.com>
2022-12-06 12:46:01 -0800

    fixed bug - added params logic to the giftpage

--- src/components/GiftPage.js
+++ src/components/GiftPage.js
@@ -1,15 +1,25 @@
 import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js"
+import { loadStripe } from "@stripe/stripe-js";
 import GiftForm from "./GiftForm";
+import { useParams } from "react-router-dom";
 
-const stripePromise = loadStripe('pk_test_51LZdCRC8F8tuRxQ8R3zRiretw4DVHplXVohSh2QVGHoGGoJCgactHjMIPV4Gupwpwnw3Q07NYixW7cchEKNHwxjs00wVIopF6E')
+const stripePromise = loadStripe(
+  "pk_test_51LZdCRC8F8tuRxQ8R3zRiretw4DVHplXVohSh2QVGHoGGoJCgactHjMIPV4Gupwpwnw3Q07NYixW7cchEKNHwxjs00wVIopF6E"
+);
 
-export default function GiftPage(props) {
-  return (
-    <Elements stripe={stripePromise}>
-      <GiftForm answer_id={props.match.params.id}/>
-    </Elements>
-  )
+export function withRouter(Children) {
+  return (props) => {
+    const match = { params: useParams() };
+    return <Children {...props} match={match} />;
+  };
 }
 
+function GiftPage(props) {
+  return (
+    <Elements stripe={stripePromise}>
+      <GiftForm answer_id={props.match.params.id} />
+    </Elements>
+  );
+}
 
+export default withRouter(GiftPage);
